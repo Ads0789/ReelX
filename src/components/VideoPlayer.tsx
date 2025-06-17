62c7bd367f157e4c1cd498aae437d28446e5065c
Ads <das07142@gmail.com>
2025-06-17 22:58:59 +0530

    delete

--- src/components/VideoPlayer.tsx
+++
@@ -1,9 +0,0 @@
-const VideoPlayerSkeleton = () => {
-  return (
-    <div className="w-full aspect-video bg-gray-200 rounded-md animate-pulse">
-      {/* Placeholder for video player */}
-    </div>
-  );
-};
-
-export default VideoPlayerSkeleton;
